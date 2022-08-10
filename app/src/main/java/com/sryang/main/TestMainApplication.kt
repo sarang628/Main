5ca2f3d635eae495d2f37db993269f94b7695392
sryang <sarang628@naver.com>
2022-08-10 09:25:29 +0900

    hiit 의존성 주입 추가 add hiit dependency injection

---
+++ app/src/main/java/com/sryang/main/TestMainApplication.kt
@@ -0,0 +1,9 @@
+package com.sryang.main
+
+import android.app.Application
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TestMainApplication : Application() {
+}
