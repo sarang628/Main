30b283e360a5f2bbb855a95405387dbb342c926f
sryang <sarang628@naver.com>
2022-08-10 09:52:08 +0900

    저장소 모듈 업데이트 Repository module update

--- app/src/main/java/com/sryang/main/di/RepositoryModule.kt
+++ app/src/main/java/com/sryang/main/di/RepositoryModule.kt
@@ -1,4 +1,4 @@
-package com.sarang.torang.di
+package com.sryang.main.di
 
 import android.content.Context
 import com.example.torang_core.data.AppDatabase
@@ -8,7 +8,6 @@ import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
