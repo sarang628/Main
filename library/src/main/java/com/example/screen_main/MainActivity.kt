e6ee8f57280411e7e114344c748d80e429455713
SRYANG <sarang628@naver.com>
2023-05-04 15:14:30 +0800

    update

--- library/src/main/java/com/example/screen_main/MainActivity.kt
+++ library/src/main/java/com/example/screen_main/MainActivity.kt
@@ -2,36 +2,13 @@ package com.example.screen_main
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.example.screen_main.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
-/**
- * [ActivityMainBinding]
- */
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    //바인딩
-//    lateinit var loginManager: LoginManager
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-//        loginManager.onActivityResult(this, requestCode, resultCode, data!!)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-//        loginManager.onDestroy(this)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        Logger.d("onOptionsItemSelected")
-        return super.onOptionsItemSelected(item)
     }
 }
