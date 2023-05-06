448612b789f336abb93359daa9cd9584b6e1d4b7
SRYANG <sarang628@naver.com>
2023-05-06 15:03:43 +0800

    update

---
+++ app/src/main/java/com/sryang/main/Test1.kt
@@ -0,0 +1,10 @@
+package com.sryang.main
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+@Module
+@InstallIn(ActivityComponent::class)
+class Test1 {
+}
--- library/src/main/java/com/example/screen_main/NavigationModule.kt
+++ library/src/main/java/com/example/screen_main/NavigationModule.kt
@@ -2,13 +2,13 @@ package com.example.screen_main
 
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import com.example.navigation.AddReviewNavigation
+//import com.example.navigation.AddReviewNavigation
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 
-@Module
+/*@Module
 @InstallIn(ActivityComponent::class)
 class NavigationModule {
     @Provides
@@ -19,4 +19,4 @@ class NavigationModule {
             }
         }
     }
-}
+}*/
