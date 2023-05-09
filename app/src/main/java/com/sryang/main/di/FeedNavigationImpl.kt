00b1324730e88a52837b2633ecb9516a4c49f56d
SRYANG <sarang628@naver.com>
2023-05-09 16:04:07 +0800

    update

---
+++ app/src/main/java/com/sryang/main/di/FeedNavigationImpl.kt
@@ -0,0 +1,39 @@
+package com.sryang.main.di
+
+import android.content.Context
+import android.view.View
+import com.example.navigation.FeedNavigations
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+@Module
+@InstallIn(ActivityComponent::class)
+class FeedNavigationImpl {
+    @Provides
+    fun a(): FeedNavigations {
+        return object : FeedNavigations {
+            override fun goAddReview(context: Context, view: View) {
+
+            }
+
+            override fun goComment(context: Context, view: View) {
+
+            }
+
+            override fun goFullImage(context: Context, view: View) {
+
+            }
+
+            override fun goProfile(context: Context, view: View) {
+
+            }
+
+            override fun goRestaurant(context: Context, view: View) {
+
+            }
+
+        }
+    }
+}
