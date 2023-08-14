43338d15a410d5a45dcd52abdc5cccf68d1f4ff0
SRYANG <sarang628@naver.com>
2023-08-14 20:47:19 +0800

    update

--- library/src/androidTest/java/com/sryang/main/LoginTest.kt
+++ library/src/androidTest/java/com/sryang/main/LoginTest.kt
@@ -1,11 +1,17 @@
 package com.sryang.main
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.sryang.library.LoginLogic
 import com.sryang.library.LoginResult
 import com.sryang.library.LogoutResult
+import org.junit.Test
+import org.junit.runner.RunWith
 
 
+@RunWith(AndroidJUnit4::class)
 class LoginTest {
+
+    @Test
     fun loginTest() {
         val logicLogic = object : LoginLogic {
             override suspend fun requestLogin(
@@ -19,7 +25,10 @@ class LoginTest {
             override suspend fun requestLogout(logoutResult: LogoutResult) {
                 TODO("Not yet implemented")
             }
-
         }
+
+        /*logicLogic.requestLogout {
+
+        }*/
     }
 }
