f0598d4d7d5a1895b810b939eacb7728cc28f978
SRYANG <sarang628@naver.com>
2023-11-28 17:17:19 +0800

    add main submodule

--- app/src/main/java/com/sryang/torang/MainActivity.kt
+++ app/src/main/java/com/sryang/torang/MainActivity.kt
@@ -8,14 +8,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
-import com.sryang.torang.compose.MainScreen
 import com.google.samples.apps.sunflower.ui.TorangTheme
-import com.sarang.alarm.compose.AlarmScreen
-import com.sryang.main.BuildConfig
-import com.sryang.screenfindingtest.di.finding.Finding
-import com.sryang.torang.compose.bottomsheet.comment.CommentBottomSheetDialog
-import com.sryang.torang.di.feed_di.FeedScreen
-import com.sryang.torang.di.main.ProvideMainScreen
+import com.sryang.torang.di.main_di.ProvideMainScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
