b75a5b6108cd69d1805273f720866d27e9abb5e2
sryang <sarang628@gmail.com>
2025-08-23 15:36:41 +0800

    add providePullToRefresh

--- app/src/main/java/com/sarang/torang/di/pulltorefresh/providePullToRefreshLayout.kt
+++ app/src/main/java/com/sarang/torang/di/pulltorefresh/providePullToRefreshLayout.kt
@@ -1,10 +1,29 @@
 package com.sarang.torang.di.pulltorefresh
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.sryang.library.pullrefresh.PullToRefreshLayout
 import com.sryang.library.pullrefresh.PullToRefreshLayoutState
+import com.sryang.library.pullrefresh.RefreshIndicatorState
 
 fun providePullToRefreshLayout(state: PullToRefreshLayoutState): @Composable ((isRefreshing: Boolean, onRefresh: (() -> Unit), contents: @Composable (() -> Unit)) -> Unit) {
     return { _, _, content ->
         content.invoke()
     }
 }
+
+fun providePullToRefresh(state: PullToRefreshLayoutState): @Composable ((modifier : Modifier, isRefreshing: Boolean, onRefresh: (() -> Unit), contents: @Composable (() -> Unit)) -> Unit) =
+    { modifier, isRefreshing, onRefresh, contents ->
+
+        if (isRefreshing) {
+            state.updateState(RefreshIndicatorState.Refreshing)
+        } else {
+            state.updateState(RefreshIndicatorState.Default)
+        }
+
+        PullToRefreshLayout(
+            modifier = Modifier, pullRefreshLayoutState = state, refreshThreshold = 80, onRefresh = onRefresh
+        ) {
+            contents.invoke()
+        }
+    }
