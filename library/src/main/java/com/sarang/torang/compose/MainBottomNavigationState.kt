a778dfe36dc9fb494905159360f6ea7efaffb5e8
sryang <sarang628@gmail.com>
2025-10-21 11:52:25 +0900

    separate MainBottomNavigationState

---
+++ library/src/main/java/com/sarang/torang/compose/MainBottomNavigationState.kt
@@ -0,0 +1,40 @@
+package com.sarang.torang.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavHostController
+import com.sarang.torang.compose.main.MainDestination
+import kotlin.reflect.KClass
+
+class MainBottomNavigationState(private val navController: NavHostController) {
+    @Composable
+    fun isSelected(destination: MainDestination) : Boolean {
+        return currentDestination
+            .isRouteInHierarchy(destination.baseRoute)
+    }
+
+    private val previousDestination = mutableStateOf<NavDestination?>(null)
+    val currentDestination: NavDestination?
+        @Composable get() {
+            // Collect the currentBackStackEntryFlow as a state
+            val currentEntry = navController.currentBackStackEntryFlow
+                .collectAsState(initial = null)
+
+            // Fallback to previousDestination if currentEntry is null
+            return currentEntry.value?.destination.also { destination ->
+                if (destination != null) {
+                    previousDestination.value = destination
+                }
+            } ?: previousDestination.value
+        }
+}
+
+
+private fun NavDestination?.isRouteInHierarchy(route: KClass<*>) =
+    this?.hierarchy?.any {
+        it.hasRoute(route)
+    } == true
